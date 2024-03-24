a3871bb7d1c601d3d17668313a9d526a513384c0
BilqisAuraa <22082010207@student.upnjatim.ac.id>
2024-03-25 00:40:45 +0700

    all update

--- JsGhostJump.js
+++ JsGhostJump.js
@@ -1,212 +1,114 @@
-//board
-let board;
-let boardWidth = 1200;
-let boardHeight = 750;
-let context;
-
-//poci
-let pociWidth = 180;
-let pociHeight = 100;
-let pociX = boardWidth/2 - pociWidth/2;
-let pociY = boardHeight*7/8 - pociHeight;
-let pociRightImg;
-let pociLeftImg;
-
-let poci = {
-    img : null,
-    x : pociX,
-    y : pociY,
-    width : pociWidth,
-    height : pociHeight
-}
-
-//physics
-let velocityX = 0; 
-let velocityY = 0; //poci jump speed
-let initialVelocityY = -8; //starting velocity Y
-let gravity = 0.4;
-
-//platforms
-let platformArray = [];
-let platformWidth = 150;
-let platformHeight = 100;
-let platformImg;
-
+const canvas = document.getElementById('board');
+let platFormGap = 0;
 let score = 0;
-let HighScore = 0;
-/*let gameOver = false;*/
+let highScore = 0;
 
-window.onload = function() {
-    board = document.getElementById("board");
-    board.height = boardHeight;
-    board.width = boardWidth;
-    context = board.getContext("2d"); //used for drawing on the board
-
-    //load images
-    pociRightImg = new Image();
-    pociRightImg.src = "PociRight.png";
-    poci.img = pociRightImg;
-    pociRightImg.onload = function() {
-        context.drawImage(poci.img, poci.x, poci.y, poci.width, poci.height);
+class Doodler {
+    constructor() {
+        this.context = canvas.getContext("2d");
+        this.x = canvas.width / 2;
+        this.y = canvas.height - 200;
+        this.image = new Image();
+        this.image.src = 'PociRight.png'
+        this.prevY = this.y;
+        this.width = 70;
+        this.height = 100;
+        this.vx = 0;
+        this.vy = 0;
+        this.gravity = 0.04;
+        this.jumpStrength = -2.5;
     }
 
-    pociLeftImg = new Image();
-    pociLeftImg.src = "PociLeft.png";
-
-    platformImg = new Image();
-    platformImg.src = "Platform.png";
-
-    velocityY = initialVelocityY;
-    placePlatforms();
-    requestAnimationFrame(update);
-    document.addEventListener("keydown", movepoci);
-}
-
-function update() {
-    requestAnimationFrame(update);
-    if (gameOver) {
-        return;
-    }
-    context.clearRect(0, 0, board.width, board.height);
-
-    //poci
-    poci.x += velocityX;
-    if (poci.x > boardWidth) {
-        poci.x = 0;
-    }
-    else if (poci.x + poci.width < 0) {
-        poci.x = boardWidth;
-    }
-
-    velocityY += gravity;
-    poci.y += velocityY;
-    if (poci.y > board.height) {
-        gameOver = true;
-    }
-    context.drawImage(poci.img, poci.x, poci.y, poci.width, poci.height);
-
-    //platforms
-    for (let i = 0; i < platformArray.length; i++) {
-        let platform = platformArray[i];
-        if (velocityY < 0 && poci.y < boardHeight*3/4) {
-            platform.y -= initialVelocityY; //slide platform down
-        }
-        if (detectCollision(poci, platform) && velocityY >= 0) {
-            velocityY = initialVelocityY; //jump
-        }
-        context.drawImage(platform.img, platform.x, platform.y, platform.width, platform.height);
-    }
-
-    // clear platforms and add new platform
-    while (platformArray.length > 0 && platformArray[0].y >= boardHeight) {
-        platformArray.shift(); //removes first element from the array
-        newPlatform(); //replace with new platform on top
-    }
-
-    //score
-    updateScore();
-    context.fillStyle = "white";
-    context.textAlign = ""
-    context.font = "30px Times New Roman";
-    context.fillText(`Score: ${Math.round(score)}`, boardWidth / 10, 50);
-}
-
-function movepoci(e) {
-    if (e.code == "ArrowRight" || e.code == "KeyD") { //gerak right
-        velocityX = 4;
-        poci.img = pociRightImg;
-    }
-    else if (e.code == "ArrowLeft" || e.code == "KeyA") { //gerak left
-        velocityX = -4;
-        poci.img = pociLeftImg;
-    }
-    document.getElementById("retry").addEventListener('click', function() {
-        hideEndMenu();
-        resetGame();
-        placePlatforms();
-    });
-    /*else if (e.code == "Space" && gameOver) {
-        //reset
-        poci = {
-            img : pociRightImg,
-            x : pociX,
-            y : pociY,
-            width : pociWidth,
-            height : pociHeight
+    updatePosition() {
+        this.prevY = this.y;
+        this.x += this.vx;
+        this.y += this.vy;
+        if(this.vy > 3.5) {
+            this.vy = 3.5;
+        } else {
+            this.vy += this.gravity;
         }
 
-        velocityX = 0;
-        velocityY = initialVelocityY;
-        score = 0;
-        maxScore = 0;
-        gameOver = false;
-        placePlatforms();
-    }*/
-}
-
-
-function placePlatforms() {
-    platformArray = [];
-
-    //start platforms
-    let platform = {
-        img : platformImg,
-        x : boardWidth/2,
-        y : boardHeight - 50,
-        width : platformWidth,
-        height : platformHeight
+        this.checkForWrapDoodler();
+        this.checkCollisionWithPlatforms();
     }
 
-    platformArray.push(platform);
-
-    for (let i = 0; i < 6; i++) {
-        let randomX = Math.floor(Math.random() * boardWidth*3/4); //(0-1) * boardWidth*3/4
-        let platform = {
-            img : platformImg,
-            x : randomX,
-            y : boardHeight - 90*i - 200,
-            width : platformWidth,
-            height : platformHeight
-        }
-    
-        platformArray.push(platform);
-    }
-}
-
-function newPlatform() {
-    let randomX = Math.floor(Math.random() * boardWidth*3/4); //(0-1) * boardWidth*3/4
-    let platform = {
-        img : platformImg,
-        x : randomX,
-        y : -platformHeight,
-        width : platformWidth,
-        height : platformHeight
-    }
-
-    platformArray.push(platform);
-}
-
-function detectCollision(a, b) {
-    return a.x < b.x + b.width &&   //a's top left corner doesn't reach b's top right corner
-           a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
-           a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
-           a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
-}
-
-function updateScore() {
-    let points = Math.floor(50*Math.random()); //(0-1) *50 --> (0-50)
-    if (velocityY < 0) { //negative going up
-        maxScore += points;
-        if (score < maxScore) {
-            score = maxScore;
+    checkForWrapDoodler() {
+        if(this.x + this.width < 0) {
+            this.x = canvas.width;
+        } else if(this.x > canvas.width) {
+            this.x = 0 - this.width;
         }
     }
-    else if (velocityY >= 0) {
-        maxScore -= points;
+
+    checkCollisionWithPlatforms() {
+        if(this.vy <= 0) {
+            return;
+        }
+
+        for(let i = 0; i < platForms.length; i++) {
+            let platform = platForms[i];
+            if(
+                (this.prevY + this.height + 20) >= platform.y &&
+                this.x + this.width > platform.x &&
+                this.x < platform.x + platform.width &&
+                this.y + this.height > platform.y &&
+                this.y < platform.y + platform.height &&
+                this.prevY < platform.y
+            ) {
+                this.jump(platform);
+            }
+        }
+    }
+
+    jump(platform) {
+        let newHeight = platform.y - this.height;
+        if(newHeight > (canvas.height / 2 - 120)) {
+            this.y = platform.y - this.height;
+            this.vy = this.jumpStrength;
+        }
+    }
+
+    moveRight() {
+        this.vx += 4;
+        this.image.src = 'PociRight.png';
+    }
+
+    moveLeft () {
+        this.vx -= 4;
+        this.image.src = 'PociLeft.png';
+    }
+
+    draw() {
+        this.context.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
 }
 
-function ShowEndMenu() {
+class Platform {
+    constructor(x, y) {
+        this.context = canvas.getContext("2d");
+        this.image = new Image();
+        this.image.src = 'Platform.png';
+        this.x = x;
+        this.y = y;
+        this.width = 100;
+        this.height = 50;
+    }
+
+    draw() {
+        this.context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+
+}
+
+let platForms = [];
+const doodler = new Doodler();
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
+
+function showEndMenu() {
     document.getElementById('end-game').style.display = 'block';
     document.getElementById('end-game-score').innerHTML = score;
 
@@ -215,9 +117,108 @@ function ShowEndMenu() {
     }
 
     document.getElementById('high-score').innerHTML = highScore;
-
 }
 
-function HideEndMenu() {
+function hideEndMenu() {
     document.getElementById('end-game').style.display = 'none';
 }
+
+function addListeners() {
+    document.addEventListener('keydown', function(event) {
+        if(event.code === "ArrowLeft") {
+            doodler.moveLeft();
+        } else {
+            doodler.moveRight();
+        }
+    });
+
+    document.addEventListener('keyup', function(event) {
+        if(event.code === "ArrowLeft" || event.code === "ArrowRight") {
+            doodler.vx = 0;
+        }
+    });
+
+    document.getElementById("retry").addEventListener('click', function() {
+        hideEndMenu();
+        resetGame();
+        loop();
+    });
+}
+
+function createPlatforms(platFormCount) {
+    platFormGap = Math.round(canvas.height / platFormCount);
+
+    for(let i = 0; i < platFormCount; i++) {
+        let xpos = 0;
+        do {
+            xpos = randomInteger(25, canvas.width - 25 - 100);
+        } while (
+            xpos > canvas.width / 2 - 100 * 1.5 &&
+            xpos < canvas.width / 2 + 100 / 2
+        )
+        let y = (canvas.height / 1.5) - i * platFormGap;
+        platForms.push(new Platform(xpos, y));   
+    }
+}
+
+function setup() {
+    platForms.push(new Platform(doodler.x, (doodler.y + 80)));
+    createPlatforms(6);
+}
+
+function resetGame() {
+    doodler.x = canvas.width / 2;
+    doodler.y = canvas.height - 100;
+    doodler.vx = 0;
+    doodler.vy = 0;
+    score = 0;
+    platForms = [];
+    setup();
+}
+
+function scoreText() {
+    doodler.context.font = '20px Arial';
+    doodler.context.fillStyle = 'white';
+    doodler.context.textAlign = 'center';
+    doodler.context.fillText(`Score: ${Math.round(score)}`, canvas.width / 2, 50);
+}
+
+function updatePlatformsAndScore() {
+    let platformsCpy = [...platForms];
+    platForms = platForms.filter(platform_ => platform_.y < canvas.height);
+    score += platformsCpy.length - platForms.length;
+}
+
+function loop() {
+    doodler.context.clearRect(0, 0, canvas.width, canvas.height);
+
+    if(doodler.y < canvas.height / 2 && doodler.vy < 0) {
+        platForms.forEach(platform => {
+            platform.y += -doodler.vy * 2;
+        });
+
+        platForms.push(new Platform(randomInteger(25, canvas.width - 25 - 100),
+            platForms[platForms.length - 1].y - platFormGap * 2));
+    }
+
+    doodler.draw();
+    doodler.updatePosition();
+
+    platForms.forEach(platform => {
+        platform.draw();
+    });
+
+    scoreText();
+    if(doodler.y > canvas.height) {
+        showEndMenu();
+        return;
+    }
+
+    updatePlatformsAndScore()
+
+    requestAnimationFrame(loop);
+}
+
+addListeners();
+setup();
+loop();
